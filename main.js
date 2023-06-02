76296173c9dadda93f08f48a4aee06886183ee72
kurosiko <101198724+kurosiko@users.noreply.github.com>
2023-06-02 17:54:45 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function del(){
+    const tweets = document.getElementsByClassName("css-901oao r-1nao33i r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-1h8ys4a r-1jeg54m r-qvutc0")
+    console.log(tweets)
+    for (const tweet of tweets){
+    tweet.remove()
+    }
+}
