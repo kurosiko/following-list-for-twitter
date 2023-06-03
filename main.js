8aa9c2c7fce55c08ebac57e9f3e3dc8e545a2977
kurosiko <ttnntt1234567890987654321@gmail.com>
2023-06-03 18:25:50 +0900

    test

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function del(){
-    const tweets = document.getElementsByClassName("css-901oao r-1nao33i r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-1h8ys4a r-1jeg54m r-qvutc0")
+    const tweets = document.getElementsByClassName("css-18t94o4 css-1dbjc4n r-1ny4l3l r-ymttw5 r-1f1sjgu r-o7ynqc r-6416eg")
+    //const tweets = document.getElementsByClassName("css-901oao r-1nao33i r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-1h8ys4a r-1jeg54m r-qvutc0")
     console.log(tweets)
     for (const tweet of tweets){
     tweet.remove()
